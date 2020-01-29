f36ccd6a1f7619a71be02a49a558906cf13e71c6
Zach Stack <stackz.personal@gmail.com>
2020-01-29 12:20:42 -0800

    Build form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'; 
+import { Container } from 'reactstrap';
 import './App.css';
 
 function App() {
+  const [teamMember, setTeamMember] = useState([])
+  const addMember = member => {
+    const newMember = {
+      id: Date.now(),
+      name: member.name,
+      email: member.email,
+      role: member.role
+    };
+    setTeamMember([...teamMember, newMember])
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className='team-container' fluid={true}>
+      
+    </Container>
+
   );
 }
 
